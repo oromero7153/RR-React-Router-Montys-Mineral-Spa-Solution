895f734b58fb575cb6e037449341316574cc89d8
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-08-28 19:50:24 -0700

    added some bootstrap css

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,26 @@
-
+import Card from 'react-bootstrap/Card'
+import { ListGroup } from 'react-bootstrap'
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+    const displayPackages = props.packages.map((eachPackage, index) => <ListGroup.Item key={index}>{eachPackage}</ListGroup.Item>)
 
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {displayPackages}
-                </ul>
+                <h1>Packages</h1>
             </div>
+            <Card style={{ width: '75%', margin: '0 auto'}}>
+                <Card.Body>
+                    <Card.Text>
+                        Check out some of our packages! Every pacakage is ethically sourced and organic!
+                    </Card.Text>
+                </Card.Body>
+                    <ListGroup>
+                     {displayPackages}   
+                    </ListGroup>
+            </Card>
+        </div>
         </div>
     )
 }
